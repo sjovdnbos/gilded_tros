5a14b702ef1c25360ac31e3ea9256d8a4ceb210a
Simon Van den Bossche <simon.vandenbossche@viumore.com>
2025-06-20 19:51:49 +0200

    smelly tests

---
+++ test/test_smelly_strategy.py
@@ -0,0 +1,49 @@
+import unittest
+from src.models.item import Item
+from src.strategies.smelly import SmellyItemUpdate
+
+class SmellyItemUpdateTests(unittest.TestCase):
+
+    def test_quality_degrades_before_expiry(self):
+        """
+        Smelly items lose 2 quality per day before sell_in date.
+        """
+        item = Item("Long Methods", sell_in=5, quality=10)
+        strategy = SmellyItemUpdate()
+        strategy.update(item)
+
+        self.assertEqual(item.sell_in, 4)
+        self.assertEqual(item.quality, 8)
+
+    def test_quality_degrades_after_expiry(self):
+        """
+        Smelly items lose 4 quality per day after sell_in date.
+        """
+        item = Item("Ugly Variable Names", sell_in=0, quality=10)
+        strategy = SmellyItemUpdate()
+        strategy.update(item)
+
+        self.assertEqual(item.sell_in, -1)
+        self.assertEqual(item.quality, 6)
+
+    def test_quality_never_negative(self):
+        """
+        Smelly item quality should never go below 0.
+        """
+        item = Item("Duplicate Code", sell_in=1, quality=1)
+        strategy = SmellyItemUpdate()
+        strategy.update(item)
+
+        self.assertEqual(item.sell_in, 0)
+        self.assertEqual(item.quality, 0)
+
+    def test_quality_never_negative_even_when_expired(self):
+        """
+        Smelly item quality should never go below 0, even after expiration.
+        """
+        item = Item("Duplicate Code", sell_in=-1, quality=3)
+        strategy = SmellyItemUpdate()
+        strategy.update(item)
+
+        self.assertEqual(item.sell_in, -2)
+        self.assertEqual(item.quality, 0)
